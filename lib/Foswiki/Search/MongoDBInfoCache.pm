c04fef56f28b34f200ddcdfac2bfb20e9060cd05
SvenDowideit <SvenDowideit@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-02-28 11:45:40 +0000

    Item8543: start to implement a MongoDB cursor infocache wrapper so that Sorting, paging etc can all be delegated to MongoDB
    git-svn-id: http://svn.foswiki.org/trunk/MongoDBPlugin@6571 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

---
+++ lib/Foswiki/Search/MongoDBInfoCache.pm
@@ -0,0 +1,342 @@
+# See bottom of file for license and copyright information
+package Foswiki::Search::MongoDBInfoCache;
+use strict;
+
+use Foswiki::Iterator ();
+our @ISA = ('Foswiki::Iterator');
+
+=begin TML
+
+---+ package Foswiki::Search::MongoDBInfoCache
+
+uses the MongoDB cursor, and sorting limiting etc to create a fastpath iterator.
+
+=cut
+
+use Assert;
+use Foswiki::Meta ();
+use Foswiki::Iterator::FilterIterator ();
+
+#use Monitor ();
+#Monitor::MonitorMethod('Foswiki::Search::MongoDBInfoCache', 'getTopicListIterator');
+
+=pod
+---++ Foswiki::Search::MongoDBInfoCache::new($session, $defaultWeb, $options, $cursor)
+
+=cut
+
+sub new {
+    my ( $class, $session, $defaultWeb, $options, $cursor ) = @_;
+    
+    #my $this = $class->SUPER::new({});
+    my $this = bless({}, $class);
+    $this->{_session}    = $session;
+    $this->{_defaultWeb} = $defaultWeb;
+    $this->{_SEARCHoptions}    = $options;
+    $this->{_cursor}    = $cursor;
+
+    return $this;
+}
+
+sub hasNext {
+    my $this = shift; 
+    return $this->{_cursor}->has_next;
+}
+
+sub next { 
+    my $this = shift; 
+    my $obj = $this->{_cursor}->next;
+    return $obj->{_topic};
+}
+sub reset { return 0; }
+sub all {die 'not implemented';}
+
+
+sub isImmutable {
+    my $this = shift;
+    return 1;
+}
+
+sub addTopics {
+    my ( $this, $defaultWeb, @list ) = @_;
+    ASSERT( !$this->isImmutable() )
+    if DEBUG  ;    #cannot modify list once its being used as an iterator.
+    ASSERT( defined($defaultWeb) ) if DEBUG;
+
+    die 'not implemented';
+}
+
+#TODO: what if it isa Meta obj
+#TODO: or an infoCache obj..
+sub addTopic {
+    my ( $this, $meta ) = @_;
+    ASSERT( !$this->isImmutable() )
+    if DEBUG  ;    #cannot modify list once its being used as an iterator.
+
+    die 'not implemented';
+}
+
+=begin TML
+---++ sortResults
+
+IMPLEMENTME
+
+=cut
+
+sub sortResults {
+    my ( $infoCache, $web, $params ) = @_;
+    my $session = $infoCache->{_session};
+return;
+    my $sortOrder = $params->{order} || '';
+    my $revSort   = Foswiki::isTrue( $params->{reverse} );
+    my $date      = $params->{date} || '';
+    my $limit     = $params->{limit} || '';
+
+    #SMELL: duplicated code - removeme
+    # Limit search results
+    if ( $limit =~ /(^\d+$)/o ) {
+
+        # only digits, all else is the same as
+        # an empty string.  "+10" won't work.
+        $limit = $1;
+    }
+    else {
+
+        # change 'all' to 0, then to big number
+        $limit = 0;
+    }
+    $limit = 32000 unless ($limit);
+    #TODO: this is really an ugly hack to get around the rather horrible limit 'performance' hack
+    if (defined($params->{pager_show_results_to}) and
+        $params->{pager_show_results_to} > 0) {
+        $limit = $params->{pager_skip_results_from} + $params->{pager_show_results_to};
+    }
+
+    # sort the topic list by date, author or topic name, and cache the
+    # info extracted to do the sorting
+    if ( $sortOrder eq 'modified' ) {
+
+        # For performance:
+        #   * sort by approx time (to get a rough list)
+        #   * shorten list to the limit + some slack
+        #   * sort by rev date on shortened list to get the accurate list
+        # SMELL: Cairo had efficient two stage handling of modified sort.
+        # SMELL: In Dakar this seems to be pointless since latest rev
+        # time is taken from topic instead of dir list.
+        my $slack = 10;
+        if ( $limit + 2 * $slack < scalar( @{ $infoCache->{list} } ) ) {
+
+            # sort by approx latest rev time
+            my @tmpList =
+              map  { $_->[1] }
+              sort { $a->[0] <=> $b->[0] }
+              map  { [ $session->getApproxRevTime( $web, $_ ), $_ ] }
+              @{ $infoCache->{list} };
+            @tmpList = reverse(@tmpList) if ($revSort);
+
+            # then shorten list and build the hashes for date and author
+            my $idx = $limit + $slack;
+            @{ $infoCache->{list} } = ();
+            foreach (@tmpList) {
+                push( @{ $infoCache->{list} }, $_ );
+                $idx -= 1;
+                last if $idx <= 0;
+            }
+        }
+
+        $infoCache->sortTopics( $sortOrder, !$revSort );
+    }
+    elsif (
+        $sortOrder =~ /^creat/ ||    # topic creation time
+        $sortOrder eq 'editby' ||    # author
+        $sortOrder =~ s/^formfield\((.*)\)$/$1/    # form field
+      )
+    {
+        $infoCache->sortTopics( $sortOrder, !$revSort );
+    }
+    else {
+
+        # simple sort, see Codev.SchwartzianTransformMisused
+        # note no extraction of topic info here, as not needed
+        # for the sort. Instead it will be read lazily, later on.
+        if ($revSort) {
+            @{ $infoCache->{list} } =
+              sort { $b cmp $a } @{ $infoCache->{list} };
+        }
+        else {
+            @{ $infoCache->{list} } =
+              sort { $a cmp $b } @{ $infoCache->{list} };
+        }
+    }
+
+    if ($date) {
+        require Foswiki::Time;
+        my @ends       = Foswiki::Time::parseInterval($date);
+        my @resultList = ();
+        foreach my $topic ( @{ $infoCache->{list} } ) {
+
+            # if date falls out of interval: exclude topic from result
+            my $topicdate = $session->getApproxRevTime( $web, $topic );
+            push( @resultList, $topic )
+              unless ( $topicdate < $ends[0] || $topicdate > $ends[1] );
+        }
+        @{ $infoCache->{list} } = @resultList;
+    }
+}
+
+
+######OLD methods
+sub get {
+    my ( $this, $topic, $meta ) = @_;
+
+    unless ($this->{$topic}) {
+        $this->{$topic} = {};
+        $this->{$topic}->{tom} = $meta || 
+          Foswiki::Meta->load( $this->{_session}, $this->{_defaultWeb},
+            $topic );
+        # SMELL: why do this here? Smells of a hack, as AFAICT it is done
+        # anyway during output processing. Disable it, and see what happens....
+        #my $text = $topicObject->text();
+        #$text =~ s/%WEB%/$web/gs;
+        #$text =~ s/%TOPIC%/$topic/gs;
+        #$topicObject->text($text);
+
+        # Extract sort fields
+        my $ri = $this->{$topic}->{tom}->getRevisionInfo();
+
+        # Rename fields to match sorting criteria
+        $this->{$topic}->{editby}   = $ri->{author} || '';
+        $this->{$topic}->{modified} = $ri->{date};
+        $this->{$topic}->{revNum}   = $ri->{version};
+
+        $this->{$topic}->{allowView} = $this->{$topic}->{tom}->haveAccess('VIEW');
+    }
+
+    return $this->{$topic};
+}
+
+# Determins, and caches, the topic revision info of the base version,
+sub getRev1Info {
+    my ( $this, $topic, $attr ) = @_;
+
+    my $info = $this->get($topic);
+    unless ( defined $info->{$attr} ) {
+        my $ri = $info->{rev1info};
+        unless ($ri) {
+            my $tmp =
+              Foswiki::Meta->load( $this->{_session}, $this->{_defaultWeb},
+                $topic, 1 );
+            $info->{rev1info} = $ri = $tmp->getRevisionInfo();
+        }
+
+        if ( $attr eq 'createusername' ) {
+            $info->{createusername} =
+              $this->{_session}->{users}->getLoginName( $ri->{author} );
+        }
+        elsif ( $attr eq 'createwikiname' ) {
+            $info->{createwikiname} =
+              $this->{_session}->{users}->getWikiName( $ri->{author} );
+        }
+        elsif ( $attr eq 'createwikiusername' ) {
+            $info->{createwikiusername} =
+              $this->{_session}->{users}->webDotWikiName( $ri->{author} );
+        }
+        elsif ( $attr eq 'createdate' or
+               $attr eq 'createlongdate' or
+               $attr eq 'created' ) {
+            $info->{created} = $ri->{date};
+            require Foswiki::Time;
+            $info->{createdate} = Foswiki::Time::formatTime( $ri->{date} );
+            #TODO: wow thats disgusting.
+            $info->{created} = $info->{createlongdate} = $info->{createdate};
+        }
+    }
+    return $info->{$attr};
+}
+
+# Sort a topic list using cached info
+sub sortTopics {
+    my ( $this, $sortfield, $revSort ) = @_;
+    ASSERT($sortfield);
+
+    ASSERT( !$this->isImmutable() )
+      ;    #cannot modify list once its being used as an iterator.
+
+    # populate the cache for each topic
+    foreach my $topic ( @{ $this->{list} } ) {
+        if ( $sortfield =~ /^creat/ ) {
+
+            # The act of getting the info will cache it
+            $this->getRev1Info( $topic, $sortfield );
+        }
+        else {
+            my $info = $this->get($topic);
+            if ( !defined( $info->{$sortfield} ) ) {
+                $info->{$sortfield} =
+                  Foswiki::Search::displayFormField( $info->{tom}, $sortfield );
+            }
+        }
+
+        # SMELL: CDot isn't clear why this is needed, but it is otherwise
+        # we end up with the users all being identified as "undef"
+        my $info = $this->get($topic);
+        $info->{editby} =
+          $info->{tom}->session->{users}->getWikiName( $info->{editby} );
+    }
+    if ($revSort) {
+        @{ $this->{list} } = map { $_->[1] }
+          sort { _compare( $b->[0], $a->[0] ) }
+          map { [ $this->{$_}->{$sortfield}, $_ ] } @{ $this->{list} };
+    }
+    else {
+        @{ $this->{list} } = map { $_->[1] }
+          sort { _compare( $a->[0], $b->[0] ) }
+          map { [ $this->{$_}->{$sortfield}, $_ ] } @{ $this->{list} };
+    }
+}
+
+# RE for a full-spec floating-point number
+our ($NUMBER);
+$NUMBER = qr/^[-+]?[0-9]+(\.[0-9]*)?([Ee][-+]?[0-9]+)?$/s;
+
+sub _compare {
+    my $x = shift;
+    my $y = shift;
+    if ( $x =~ /$NUMBER/o && $y =~ /$NUMBER/o ) {
+
+        # when sorting numbers do it largest first; this is just because
+        # this is what date comparisons need.
+        return $y <=> $x;
+    }
+    else {
+        return $y cmp $x;
+    }
+}
+
+#TODO: might use this to optimise for mongoBD regex, or might push to core..
+sub convertTopicPatternToRegex {
+    my ($topic) = @_;
+    return '' unless ($topic);
+
+    # 'Web*, FooBar' ==> ( 'Web*', 'FooBar' ) ==> ( 'Web.*', "FooBar" )
+    my @arr =
+      map { s/[^\*\_\-\+$Foswiki::regex{mixedAlphaNum}]//go; s/\*/\.\*/go; $_ }
+      split( /(?:,\s*|\|)/, $topic );
+    return '' unless (@arr);
+
+    # ( 'Web.*', 'FooBar' ) ==> "^(Web.*|FooBar)$"
+    return '^(' . join( '|', @arr ) . ')$';
+}
+
+1;
+__END__
+
+# Plugin for Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+#
+# Copyright 2010 - SvenDowideit@fosiki.com
+#
+# MongoDBPlugin is # This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# For licensing info read LICENSE file in the root of this distribution.
--- lib/Foswiki/Store/SearchAlgorithms/MongoDB.pm
+++ lib/Foswiki/Store/SearchAlgorithms/MongoDB.pm
@@ -4,7 +4,7 @@ package Foswiki::Store::SearchAlgorithms::MongoDB;
 
 use strict;
 use Assert;
-use Foswiki::Search::InfoCache;
+use Foswiki::Search::MongoDBInfoCache;
 
 =begin TML
 
@@ -48,13 +48,13 @@ sub search {
 
     my $cursor =
       doMongoSearch( $web, $options, '_text', $searchString );
+    return new Foswiki::Search::MongoDBInfoCache($Foswiki::Plugins::SESSION, $web, $options, $cursor);
 
-    #TODO: this will go into the custom TopicSet
-    while ( my $topic = $cursor->next ) {
-        $seen{ $topic->{_topic} } = 1;
-    }
-
-    return \%seen;
+#    while ( my $topic = $cursor->next ) {
+#        $seen{ $topic->{_topic} } = 1;
+#    }
+#
+#    return \%seen;
 }
 
 =begin TML
@@ -155,17 +155,17 @@ sub query {
         
     my $cursor =
       doMongoSearch( $web, $options, \%elements);
+    return new Foswiki::Search::MongoDBInfoCache($Foswiki::Plugins::SESSION, $web, $options, $cursor);
 
-    my @answer;
-    while ( my $topic = $cursor->next ) {
-        push(@answer, $topic->{_topic});
-    }
-
-    $topicSet =
-      new Foswiki::Search::InfoCache( $Foswiki::Plugins::SESSION, $web,
-        \@answer );
-
-    return $topicSet;
+#    my @answer;
+#    while ( my $topic = $cursor->next ) {
+#        push(@answer, $topic->{_topic});
+#    }
+#
+#    $topicSet =
+#      new Foswiki::Search::InfoCache( $Foswiki::Plugins::SESSION, $web,
+#        \@answer );
+#    return $topicSet;
 }
 
 sub doMongoSearch {
@@ -250,27 +250,12 @@ HERE
 
 1;
 __DATA__
+# Plugin for Foswiki - The Free and Open Source Wiki, http://foswiki.org/
 #
-# Copyright (C) 2008-2009 Foswiki Contributors. All Rights Reserved.
-# Foswiki Contributors are listed in the AUTHORS file in the root
-# of this distribution. NOTE: Please extend that file, not this notice.
+# Copyright 2010 - SvenDowideit@fosiki.com
 #
-# Additional copyrights apply to some or all of the code in this
-# file as follows:
-#
-# Copyright (C) 2007 TWiki Contributors. All Rights Reserved.
-# TWiki Contributors are listed in the AUTHORS file in the root
-# of this distribution. NOTE: Please extend that file, not this notice.
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version. For
-# more details read LICENSE in the root of this distribution.
-#
-# This program is distributed in the hope that it will be useful,
+# MongoDBPlugin is # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 #
-# As per the GPL, removal of this notice is prohibited.
-#
+# For licensing info read LICENSE file in the root of this distribution.
